92b1ff61c0a455a2d8cb155d14938fd8d6f25082
javieryz <javieryezhang@gmail.com>
2023-05-25 22:56:44 +0200

    Finished docker-compose and k8s

--- subsistema_4/schemas/database.py
+++ subsistema_4/schemas/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 username = 'root'
 password = 'aos2023'
-host = 'db_database_4'
+host = 'db-subsistema-4'
 port = '3306'
 database = 'aos2023'
 
